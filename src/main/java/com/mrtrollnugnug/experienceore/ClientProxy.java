61bbcbbde927fe7fb499c392f0d921248fc4e06c
MrTrollNugNug <cazawadzki@comcast.net>
2016-11-30 16:43:32 -0500

    Added Texture + Fixed jsons

--- src/main/java/com/mrtrollnugnug/experienceore/ClientProxy.java
+++ src/main/java/com/mrtrollnugnug/experienceore/ClientProxy.java
@@ -3,7 +3,7 @@ package com.mrtrollnugnug.experienceore;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.SERVER)
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
 
 	@Override
