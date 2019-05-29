5920a7eb255a3c94f00eb7fcc36df763a55ad61e
Alex Ford <gordion42@gmail.com>
2019-05-29 07:38:54 -0600

    Fix creative tab to be with other ores

--- src/main/java/com/cazsius/experienceore/BlockExperienceOre.java
+++ src/main/java/com/cazsius/experienceore/BlockExperienceOre.java
@@ -5,7 +5,6 @@ import java.util.Random;
 import net.minecraft.block.BlockOre;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -13,7 +12,6 @@ import net.minecraft.world.IBlockAccess;
 public class BlockExperienceOre extends BlockOre {
 
 	public BlockExperienceOre() {
-		setCreativeTab(CreativeTabs.MATERIALS);
 		setSoundType(SoundType.STONE);
 		setRegistryName("experienceore:experience_ore");
 		setTranslationKey("experience_ore");
