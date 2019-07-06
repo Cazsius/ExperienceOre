33fa8df95172b22a2ccadd44406f106b1bc7969a
ProxyNeko <ProxyNeko@users.noreply.github.com>
2019-07-07 00:22:44 +0100

    Some cleanup and stuff, 1.12.2 tested and seems ready for release.

--- src/main/java/com/cazsius/experienceore/WorldGen.java
+++ src/main/java/com/cazsius/experienceore/WorldGen.java
@@ -1,49 +1,49 @@
 package com.cazsius.experienceore;
 
-import java.util.Random;
-
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
+import java.util.Random;
+
 public class WorldGen implements IWorldGenerator {
 
-		private WorldGenerator genExperienceOre;
-		public WorldGen() {
+	private WorldGenerator genExperienceOre;
 
-			// Vanilla default prob 10
-			this.genExperienceOre = new WorldGenMinable(ExperienceOre.blockExperienceOre.getDefaultState(), ExperienceOre.Config.veinSize);
-		    
-		}
-	    private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
-		    if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
-		        throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+	public WorldGen() {
+		// Vanilla default prob 10
+		this.genExperienceOre = new WorldGenMinable(ExperienceOre.blockExperienceOre.getDefaultState(), ExperienceOre.ModConfig.veinSize);
+	}
 
-		    int heightDiff = maxHeight - minHeight + 1;
-		    for (int i = 0; i < chancesToSpawn; i ++) {
-		        int x = chunk_X * 16 + rand.nextInt(16);
-		        int y = minHeight + rand.nextInt(heightDiff);
-		        int z = chunk_Z * 16 + rand.nextInt(16);
-		        generator.generate(world, rand, new BlockPos(x, y, z));
-		    }
+	private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+		if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight) {
+			throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
 		}
 
-		@Override
-		public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-				IChunkProvider chunkProvider) {
-			  switch (world.provider.getDimension()) {
-			    case 0: //Overworld
-			        this.runGenerator(this.genExperienceOre, world, random, chunkX, chunkZ, ExperienceOre.Config.spawnChance, ExperienceOre.Config.minHeight, ExperienceOre.Config.maxHeight);
-			        break;
-				case -1: // Nether
+		int heightDiff = maxHeight - minHeight + 1;
+		for (int i = 0; i < chancesToSpawn; i++) {
+			int x = chunk_X * 16 + rand.nextInt(16);
+			int y = minHeight + rand.nextInt(heightDiff);
+			int z = chunk_Z * 16 + rand.nextInt(16);
+			generator.generate(world, rand, new BlockPos(x, y, z));
+		}
+	}
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+		switch (world.provider.getDimension()) {
+			case 0: //Overworld
+				this.runGenerator(this.genExperienceOre, world, random, chunkX, chunkZ, ExperienceOre.ModConfig.spawnChance, ExperienceOre.ModConfig.minHeight, ExperienceOre.ModConfig.maxHeight);
+				break;
+			case -1: // Nether
 
 				break;
-				case 1: // End
-					break;
-				}
+			case 1: // End
+				break;
 		}
+	}
 }
