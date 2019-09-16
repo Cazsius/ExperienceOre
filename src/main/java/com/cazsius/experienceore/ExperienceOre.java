d214098929ef5ec2fed8e8f8891e0e2798a641d5
Cazsius <cazsius0518@gmail.com>
2019-09-16 18:32:22 -0400

    Began adding WorldGen

--- src/main/java/com/cazsius/experienceore/ExperienceOre.java
+++ src/main/java/com/cazsius/experienceore/ExperienceOre.java
@@ -6,9 +6,16 @@ import com.cazsius.experienceore.proxy.ClientProxy;
 import com.cazsius.experienceore.proxy.IProxy;
 import com.cazsius.experienceore.proxy.ServerProxy;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
@@ -17,7 +24,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,6 +35,8 @@ public class ExperienceOre {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private static Biome biome;
+
     public ExperienceOre() {
 
         ModLoadingContext modLoadingContext = ModLoadingContext.get();
@@ -37,8 +46,8 @@ public class ExperienceOre {
         modLoadingContext.registerConfig(ModConfig.Type.COMMON, ConfigData.COMMON_SPEC);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-    }
+
+    private void setup(final FMLCommonSetupEvent event) {}
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
@@ -50,6 +59,15 @@ public class ExperienceOre {
         @SubscribeEvent
         public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
             event.getRegistry().register(new BlockItem(ModBlocks.EXPERIENCE_ORE_BLOCK, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName("experienceore"));
+        }
+
+        @SubscribeEvent
+        public static void onFeatureRegistryEvent(RegistryEvent.Register<Feature<?>> event) {
+            ForgeRegistries.BIOMES.forEach(biome -> biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                    Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.COAL_ORE.getDefaultState(),
+                            ConfigData.COMMON.maxVeinSize.get()), Placement.COUNT_RANGE, new CountRangeConfig(100,
+                            ConfigData.COMMON.minSpawnHeight.get(), 0, ConfigData.COMMON.maxSpawnHeight.get()))));
+
         }
     }
 }
